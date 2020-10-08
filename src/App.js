136b738fea3dc981b1495432b81b15054ced592b
modu <taxfree9090@gmail.com>
2020-10-08 17:51:51 +0900

    test: backup project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<CKEditor
+				editor={ClassicEditor}
+				data='<p>Hello from CKEditor 5!</p>'
+				onInit={(editor) => {
+					// You can store the "editor" and use when it is needed.
+					console.log('Editor is ready to use!', editor);
+				}}
+				onChange={(event, editor) => {
+					const data = editor.getData();
+					console.log({ event, editor, data });
+				}}
+				onBlur={(event, editor) => {
+					console.log('Blur.', editor);
+				}}
+				onFocus={(event, editor) => {
+					console.log('Focus.', editor);
+				}}
+			/>
+		</div>
+	);
 }
 
 export default App;
